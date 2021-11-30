d4dcd3d8db2a8c919f2c4ccf1bd7f193afe30876
JesusCotes <jmcgarcia93@gmail.com>
2021-11-29 21:48:42 -0500

    Feature: Implementing overworld Scene & FPS System

---
+++ Assets/Scripts/General/PlayingGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayingGame : MonoBehaviour {
+
+    public GameObject UIcharacter;
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = UIcharacter.GetComponent<Animator>();
+        anim.SetInteger("index", DataScenes.instance.GetIndex());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
